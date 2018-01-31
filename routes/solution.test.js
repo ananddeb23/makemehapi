8be673aef903f9b974a629d03541527a46adc792
anand <ananddeb23@gmail.com>
2018-01-31 21:25:01 +0530

    Test written for handlingexercise 3

--- routes/solution.test.js
+++ routes/solution.test.js
@@ -1,31 +1,44 @@
 
+const axios = require('axios');
+const server = require('./solution');
 
-const Server = require('./solution');
-
-describe('ping controller', () => {
-  const options = {
-    method: 'GET',
-    url: 'http://localhost:9080/anand',
-  };
-
-  beforeAll((done) => {
-    Server.on('start', () => {
-      done();
-    });
+axios.get('http://localhost:9080/anand')
+  .then((response) => {
+    if (response.data === 'Hello anand') {
+      console.log(response.data, 'Success');
+    }
+    // console.log(response);
+    server.stop();
+  })
+  .catch((error) => {
+    console.log(error);
+    server.stop();
   });
 
-  afterAll((done) => {
-    Server.on('stop', () => {
-      done();
-    });
-    Server.stop();
-  });
-
-  test('responds with success for ping', (done) => {
-    Server.inject(options, (response) => {
-      // expect(response.statusCode).toBe(200);
-      expect(response.data).toBe('Hello anand');
-      done();
-    });
-  });
-});
+// describe('ping controller', () => {
+//   const options = {
+//     method: 'GET',
+//     url: 'http://localhost:9080/anand',
+//   };
+//
+//   beforeAll((done) => {
+//     Server.on('start', () => {
+//       done();
+//     });
+//   });
+//
+//   afterAll((done) => {
+//     Server.on('stop', () => {
+//       done();
+//     });
+//     Server.stop();
+//   });
+//
+//   test('responds with success for ping', (done) => {
+//     Server.inject(options, (response) => {
+//       // expect(response.statusCode).toBe(200);\
+//       expect(response.data).toBe('Hello anand');
+//       done();
+//     });
+//   });
+// });
