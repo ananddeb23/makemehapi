e8c1e8d6d390b39b5ee5f8852ffbf8187caa85bb
anand <ananddeb23@gmail.com>
2018-02-01 17:23:11 +0530

    Test for exercise one improved

--- hello1/solution.test.js
+++ hello1/solution.test.js
@@ -2,36 +2,46 @@ const http = require('http');
 const axios = require('axios');
 const server = require('./solution');
 
-// describe(' Test the output', () => {
-//   test(' xepect to get Hello Hapi', () => {
-//     axios.get('http://localhost:8080')
-//       .then((response) => {
-//         console.log(response.data);
-//         server.stop();
-//         expect(response.data).toBe('Hello hapii');
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   });
-// });
-// server.stop();
-describe(' Test the output', () => {
-  test(' xepect to get Hello Hapi', (done) => {
-    http.get('http://localhost:8080', (response) => {
-      response.setEncoding('UTF8');
+// const Server = require('./hello_hapi');
 
-      response.on('data', (data) => {
-      // console.log(data);
-      // console.log(data);
-        server.stop();
-        echo(data);
-        expect(data).toBe('Hello hapi');
-        done();
-      });
+describe('controller', () => {
+  test('responds with success for ping', (done) => {
+    server.inject('/', (response) => {
+      expect(response.result).toBe('Hello hapi');
+      done();
     });
   });
 });
+// // describe(' Test the output', () => {
+// //   test(' xepect to get Hello Hapi', () => {
+// //     axios.get('http://localhost:8080')
+// //       .then((response) => {
+// //         console.log(response.data);
+// //         server.stop();
+// //         expect(response.data).toBe('Hello hapii');
+// //       })
+// //       .catch((error) => {
+// //         console.log(error);
+// //       });
+// //   });
+// // });
+// // server.stop();
+// describe(' Test the output', () => {
+//   test(' xepect to get Hello Hapi', (done) => {
+//     http.get('http://localhost:8080', (response) => {
+//       response.setEncoding('UTF8');
+//
+//       response.on('data', (data) => {
+//       // console.log(data);
+//       // console.log(data);
+//         server.stop();
+//         echo(data);
+//         expect(data).toBe('Hello hapi');
+//         done();
+//       });
+//     });
+//   });
+// });
 
 // axios.get('http://localhost:8080')
 //   .then((response) => {
