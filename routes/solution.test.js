3a126a2d3b469f25e2fd53d87ed6f5a4d8c63c08
anand <ananddeb23@gmail.com>
2018-02-02 08:18:28 +0530

    Improved test in routes

--- routes/solution.test.js
+++ routes/solution.test.js
@@ -1,20 +1,31 @@
+//
+// const axios = require('axios');
+// const server = require('./solution');
 
-const axios = require('axios');
+// axios.get('http://localhost:9080/anand')
+//   .then((response) => {
+//     if (response.data === 'Hello anand') {
+//       console.log(response.data, 'Success');
+//     }
+//     // console.log(response);
+//     server.stop();
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     server.stop();
+//   });
 const server = require('./solution');
 
-axios.get('http://localhost:9080/anand')
-  .then((response) => {
-    if (response.data === 'Hello anand') {
-      console.log(response.data, 'Success');
-    }
-    // console.log(response);
-    server.stop();
-  })
-  .catch((error) => {
-    console.log(error);
-    server.stop();
-  });
+// const Server = require('./hello_hapi');
 
+describe('controller', () => {
+  test('responds with success for ping', (done) => {
+    server.inject('/anand', (response) => {
+      expect(response.result).toBe('Hello anand');
+      done();
+    });
+  });
+});
 // describe('ping controller', () => {
 //   const options = {
 //     method: 'GET',
